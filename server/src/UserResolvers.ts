90a61116d5ef8e95788d4ee362c24d8aa6ea2215
Yang Xiang <james891013@outlook.com>
2020-07-23 16:06:49 +0200

    add signup and login mutation, experiment with authentication

---
+++ server/src/UserResolvers.ts
@@ -0,0 +1,73 @@
+import { Resolver, Query, Mutation, Arg, ObjectType, Field, Ctx, UseMiddleware } from "type-graphql";
+import { hash, compare } from "bcryptjs";
+import { User } from "./entity/User";
+import { MyContext } from "./MyContext";
+import { createRefreshToken, createAccessToken } from "./auth";
+import { isAuth } from "./isAuthMiddleWare";
+
+@ObjectType()
+class LoginResponse {
+    @Field()
+    accessToken: string
+}
+
+@Resolver()
+export class UserResolver {
+    @Query(() => String)
+    hello() {
+        return "hi"
+    }
+
+    @Query(() => String)
+    @UseMiddleware(isAuth)
+    bye(
+        @Ctx() { payload }: MyContext
+    ) {
+        return `Your user id is ${payload!.userId}`
+    } 
+
+    @Query(() => [User])
+    users() {
+        return User.find();
+    }
+
+    @Mutation(() => LoginResponse)
+    async login(
+        @Arg("email") email: string,
+        @Arg("password") password: string,
+        @Ctx() { res }: MyContext
+    ): Promise<LoginResponse>{
+        const user = await User.findOne({ where: { email }});
+        if (!user) {
+            throw new Error("could not find user");
+        }
+        const valid = await compare(password, user.password);
+
+        if (!valid) {
+            throw new Error("bad password");
+        }
+
+        res.cookie("jid", createRefreshToken(user), { httpOnly: true });
+        return {
+            accessToken: createAccessToken(user)
+        }
+    }
+
+    @Mutation(() => Boolean)
+    async register(
+        @Arg("email") email: string,
+        @Arg("password") password: string,
+    ) {
+        const hashedPassword = await hash(password, 12);
+        try {
+            await User.insert({
+                email,
+                password: hashedPassword
+            });
+        } catch (err) {
+            console.log(err);
+            return false;
+        }
+        return true;
+    }
+}
