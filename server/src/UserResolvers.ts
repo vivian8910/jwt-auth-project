3aa6e97345fe78aedd088eebc454810e7595b039
Yang Xiang <james891013@outlook.com>
2020-07-24 16:07:26 +0200

    implementing how to revoke a user's refresh token by incrementing the token version

--- server/src/UserResolvers.ts
+++ server/src/UserResolvers.ts
@@ -1,10 +1,11 @@
-import { Resolver, Query, Mutation, Arg, ObjectType, Field, Ctx, UseMiddleware } from "type-graphql";
+import { Resolver, Query, Mutation, Arg, ObjectType, Field, Ctx, UseMiddleware, Int } from "type-graphql";
 import { hash, compare } from "bcryptjs";
 import { User } from "./entity/User";
 import { MyContext } from "./MyContext";
 import { createRefreshToken, createAccessToken } from "./auth";
 import { isAuth } from "./isAuthMiddleWare";
 import { sendRefreshToken } from "./sendRefreshToken";
+import { getConnection } from "typeorm";
 
 @ObjectType()
 class LoginResponse {
@@ -30,6 +31,13 @@ export class UserResolver {
     @Query(() => [User])
     users() {
         return User.find();
+    }
+
+    // preferably a function instead of a mutation
+    @Mutation(() => Boolean)
+    async revokeRefreshTokensForUser(@Arg("userId", () => Int) userId: number) {
+        await getConnection().getRepository(User).increment({ id: userId }, "tokenVersion", 1);
+        return true;
     }
 
     @Mutation(() => LoginResponse)
