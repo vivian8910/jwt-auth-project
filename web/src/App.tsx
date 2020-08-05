5ac3a3ce074030a6b8eb0493142f8f6ed1feeca0
Yang Xiang <james891013@outlook.com>
2020-08-05 19:17:41 +0200

    persisting user seession

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,35 +1,55 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { Register } from './pages/Register';
 import { Login } from './pages/Login';
 import { Bye } from './pages/Bye';
+import { setAccessToken } from './accessToken';
 
-const App: React.FC = () => (
-  <BrowserRouter>
-    <div>
-      <header>
-        <div>
-          <Link to="/">home</Link>
-        </div>
-        <div>
-          <Link to="/register">register</Link>
-        </div>
-        <div>
-          <Link to="/login">login</Link>
-        </div>
-        <div>
-          <Link to="/bye">bye</Link>
-        </div>
-      </header>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/register" component={Register} />
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/bye" component={Bye} />
-      </Switch>
-    </div>
-  </BrowserRouter>
-);
+const App: React.FC = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch('http://localhost:4000/refresh_token', {
+      method: 'POST',
+      credentials: 'include',
+    }).then(async (res) => {
+      const { accessToken } = await res.json();
+      setAccessToken(accessToken);
+      setLoading(false);
+    });
+  }, []);
+
+  if (loading) {
+    return <div>loading...</div>;
+  }
+
+  return (
+    <BrowserRouter>
+      <div>
+        <header>
+          <div>
+            <Link to="/">home</Link>
+          </div>
+          <div>
+            <Link to="/register">register</Link>
+          </div>
+          <div>
+            <Link to="/login">login</Link>
+          </div>
+          <div>
+            <Link to="/bye">bye</Link>
+          </div>
+        </header>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/bye" component={Bye} />
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
+};
 
 export default App;
