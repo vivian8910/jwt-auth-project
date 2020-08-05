0965b09f751708610b063bf3cdc71659cf0cbd5a
Yang Xiang <james891013@outlook.com>
2020-08-05 19:19:16 +0200

    formatting

--- server/src/auth.ts
+++ server/src/auth.ts
@@ -1,6 +1,8 @@
 import { sign } from 'jsonwebtoken';
 import { User } from './entity/User';
 
-export const createAccessToken = (user: User) => sign({ userId: user.id }, process.env.ACCESS_TOKEN_SECRET!, { expiresIn: '15m' });
+export const createAccessToken = (user: User) =>
+  sign({ userId: user.id }, process.env.ACCESS_TOKEN_SECRET!, { expiresIn: '15s' });
 
-export const createRefreshToken = (user: User) => sign({ userId: user.id, tokenVersion: user.tokenVersion }, process.env.REFRESH_TOKEN_SECRET!, { expiresIn: '7d' });
+export const createRefreshToken = (user: User) =>
+  sign({ userId: user.id, tokenVersion: user.tokenVersion }, process.env.REFRESH_TOKEN_SECRET!, { expiresIn: '7d' });
